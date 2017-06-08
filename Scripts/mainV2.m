a1b099afdcbf572be86aedc4c7f083bc7e620c81
Lac Louis <lac.louis5@gmail.com>
2017-06-08 12:56:52 +0200

    Code simplification
    Conception of two functions to create and save videos easily.

--- Scripts/mainV2.m
+++ Scripts/mainV2.m
@@ -2,34 +2,17 @@
 clear
 close all
 clc
-   
 
-video = Vid;
-vidIn = VideoReader('/Users/laclouis5/Documents/Etudes/Enseirb-Matmeca/Stages/Stage_2A/Project/findRotationAndScale/Videos/IMG_6918.mov');
-vidOut = VideoWriter('Out.mp4', 'MPEG-4');
+%% Parameters
 videoSize = [540 960];
-
 step = 1;
 
-%%
-while hasFrame(vidIn)
-    
-    frame = readFrame(vidIn);
-    
-    img = Img(frame);
-    video.addImg(img);
-end
+%% Creation
+video = createVideo('/Users/laclouis5/Documents/Etudes/Enseirb-Matmeca/Stages/Stage_2A/Project/findRotationAndScale/Videos/IMG_6918.mov');
 
-%%
+%% Correction
 video.corrAngle(step);
 video.showAngle;
 
-%%
-open(vidOut);
-
-for i = 1:1:video.nbImg
-    video.frames{i}.img = cropImg(video.frames{i}.img, videoSize);
-    writeVideo(vidOut, video.frames{i}.img); 
-end
-
-close(vidOut);
+%% Save
+% saveVideo(video);
