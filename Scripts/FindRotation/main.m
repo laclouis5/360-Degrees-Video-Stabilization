904ceb3d9b42ef7470e2dfd5333d656dfb2be462
Lac Louis <lac.louis5@gmail.com>
2017-07-26 16:37:08 +0200

    Assembly
    Assembly of the two algorithms into one

--- Scripts/FindRotation/main.m
+++ Scripts/FindRotation/main.m
@@ -4,45 +4,57 @@
 % 4th year of Engineering studies internship
 % Stellenbosch University, South Africa
 
-%% 
+%%
 clear
 close all
 clc
 
-%% Init
-path      = '/Users/laclouis5/Downloads/vid1.mp4';
+%% Param
+path      = '/Users/laclouis5/Downloads/Videos_thetaSC/IMG_7276.MP4';
 ratio     = 4/3;
-videoSize = [720, 720*ratio];
+def       = 720;
+videoSize = [def, def*ratio];
+angle     = 120;
+sc        = 1;
+step      = 2;
 
-sc   = 1;
-step = 2;
+%%
+mainVideo = VideoReader(path);
+
+vid1 = VideoWriter('/Users/laclouis5/Downloads/vid1', 'MPEG-4');
+
+[x, y, z]    = getRectWindow(angle, ratio, def);
+[x1, y1, z1] = moveWindow(x, y, z, 0, 0, -pi/2);
+
+[az, el]   = cart2sphPixel(mainVideo.Height, mainVideo.Width, x, y, z);
+[az1, el1] = cart2sphPixel(mainVideo.Height, mainVideo.Width, x1, y1, z1);
+
+%%
+open(vid1);
+while hasFrame(mainVideo)
+    
+    frame = readFrame(mainVideo);
+    imgS  = getImage(frame, az, el);
+    imgS1 = getImage(frame, az1, el1);
+    
+    writeVideo(vid1, imgS);
+end
+close(vid1);
 
 %% Main
 video1 = Vid(path, step, sc, videoSize);
 video1.corrAngle('mean');
-video1.saveVideo('/Users/laclouis5/Downloads/Out1.mp4');
 
 sumAngle1 = video1.sumAngle;
-%% Show
-figure, video1.showAngle;
-grid minor;
-figure, video1.showSumAngle;
-grid minor;
 
 %% Correct the second one
-ratio = 4/3;
-def   = 720;
-angle = 120;
-
-p   = '/Users/laclouis5/Downloads/Videos_thetaSC/IMG_7276.MP4';
-vid = VideoReader(p);
+vid = VideoReader(path);
 
 vidF = VideoWriter('/Users/laclouis5/Downloads/vidF', 'MPEG-4');
 
 [x, y, z] = getRectWindow(angle, ratio, def);
 
 open(vidF);
-
 for i = 1:video1.nbImg
     
     [X, Y, Z] = moveWindow(x, y, z, 0, deg2rad(video1.sumAngle(i)), -pi/2);
@@ -52,27 +64,21 @@ for i = 1:video1.nbImg
     
     writeVideo(vidF, img);
 end
-
 close(vidF);
 
-%% 
-path   = '/Users/laclouis5/Downloads/vidF.mp4';
-video1 = Vid(path, step, sc, videoSize);
+%%
+video1 = Vid('/Users/laclouis5/Downloads/vidF.mp4', step, sc, videoSize);
 video1.corrAngle('mean');
-video1.saveVideo('/Users/laclouis5/Downloads/Out2.mp4');
 
 sumAngle2 = video1.sumAngle;
 
 %% Correct the second one
-p   = '/Users/laclouis5/Downloads/Videos_thetaSC/IMG_7276.MP4';
-vid = VideoReader(p);
-
+vid  = VideoReader(path);
 vidF = VideoWriter('/Users/laclouis5/Downloads/vidF2', 'MPEG-4');
 
 [x, y, z] = getRectWindow(angle, ratio, def);
 
 open(vidF);
-
 for i = 1:video1.nbImg
     
     [X, Y, Z] = moveWindow(x, y, z, deg2rad(sumAngle2(i)), deg2rad(sumAngle1(i)), -pi/2);
@@ -82,5 +88,4 @@ for i = 1:video1.nbImg
     
     writeVideo(vidF, img);
 end
-
 close(vidF);
